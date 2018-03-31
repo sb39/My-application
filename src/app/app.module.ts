4e5cf4fbb42143def39fd24b64505095e3b68e4a
#Srij_b <biswassrijeet99@gmail.com>
2018-03-31 23:57:00 +0530

    initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { GettingStartedComponent } from './getting-started/getting-started.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GettingStartedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path : 'app-getting-started',
+        component : GettingStartedComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
